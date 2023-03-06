b252ef5a6074abe39bbcad090a8752f1529681d4
Anastasya Boltach <feldmiller2226@gmail.com>
2023-03-07 01:33:14 +0300

    fix layout & add function for select nationality

--- src/js/modules/index.js
+++ src/js/modules/index.js
@@ -1,88 +1,30 @@
 
-import { html } from './../helpers/elementsNodeList'
+let f = document.querySelector('#nationalites');
 
-      import * as bootstrap from 'bootstrap'
+function getNationalities() {
+  const nationalities = [
+    'American', 
+    'British', 
+    'Canadian', 
+    'Chinese', 
+    'French', 
+    'German', 
+    'Indian', 
+    'Italian', 
+    'Japanese', 
+    'Mexican', 
+    'Russian', 
+    'Spanish', 
+    'Other'
+  ];
 
+  const nationalitiesOptions = nationalities.map((nationality) => {
+    return `<option value=${nationality}>${nationality}</option>`;
+  });
 
-// logger (Full Logging System) ==========================================================================
-function FLS(message) {
-  setTimeout(() => (window.FLS ? console.log(message) : null), 0)
+  f.innerHTML = nationalitiesOptions.join('');
 }
-
-// Проверка браузера на поддержку .webp изображений ======================================================
-function isWebp() {
-  // Проверка поддержки webp
-  const testWebp = (callback) => {
-    const webP = new Image()
-
-    webP.onload = webP.onerror = () => callback(webP.height === 2)
-    webP.src =
-      'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA'
-  }
-  // Добавление класса _webp или _no-webp для HTML
-  testWebp((support) => {
-    const className = support ? 'webp' : 'no-webp'
-    html.classList.add(className)
-
-    FLS(support ? 'webp поддерживается' : 'webp не поддерживается')
-  })
-}
-
-/* Проверка мобильного браузера */
-const isMobile = {
-  Android: () => navigator.userAgent.match(/Android/i),
-  BlackBerry: () => navigator.userAgent.match(/BlackBerry/i),
-  iOS: () => navigator.userAgent.match(/iPhone|iPad|iPod/i),
-  Opera: () => navigator.userAgent.match(/Opera Mini/i),
-  Windows: () => navigator.userAgent.match(/IEMobile/i),
-  any: () =>
-    isMobile.Android() ||
-    isMobile.BlackBerry() ||
-    isMobile.iOS() ||
-    isMobile.Opera() ||
-    isMobile.Windows(),
-}
-/* Добавление класса touch для HTML если браузер мобильный */
-function addTouchClass() {
-  // Добавление класса _touch для HTML если браузер мобильный
-  if (isMobile.any()) {
-    html.classList.add('touch')
-  }
-}
-
-// Добавление loaded для HTML после полной загрузки страницы
-function addLoadedClass() {
-  window.addEventListener('load', () => {
-    setTimeout(() => {
-      html.classList.add('loaded')
-    }, 0)
-  })
-}
-
-// Получение хеша в адресе сайта
-const getHash = () => {
-  if (location.hash) {
-    return location.hash.replace('#', '')
-  }
-}
-
-// Указание хеша в адресе сайта
-function setHash(hash) {
-  hash = hash ? `#${hash}` : window.location.href.split('#')[0]
-  history.pushState('', '', hash)
-}
-
-
 
 export {
-  FLS,
-  isWebp,
-  isMobile,
-  addTouchClass,
-  addLoadedClass,
-  getHash,
-  setHash,
-  menuInit,
-  menuOpen,
-  menuClose,
+  getNationalities,
 }
